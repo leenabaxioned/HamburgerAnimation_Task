8b58703418077d912ef367b3f19bcce2c4b1dd9e
leenabaxioned <leenab@axioned.com>
2024-06-07 16:42:55 +0530

    script added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,5 @@
 /* Author: */
+var hamburger = document.querySelector('#hamburger');
+var nav = document.querySelector('#navigation');
+
+hamburger.addEventListener('click', () => nav.classList.toggle('active'));
